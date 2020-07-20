3d22eed408680544bf665f3298bca8451d5a25b3
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-20 17:45:00 -0400

    Solution to exercise 6

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = (props) => {
-  return <footer>{/* Your code here! */}</footer>;
+  return <footer><input className='message-input'></input></footer>;
 };
 
 export default Footer;
